101c6757fd73cc837d238e727c4a92016ae86587
Nathaniel Churchill <nchurchill@metrostarsystems.com>
2021-03-02 16:00:56 -0500

    Updates to code

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace CicdApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello CICD Pipelines!");
                 });
             });
         }
