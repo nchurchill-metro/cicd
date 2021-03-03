3429ee07288c65541b1e003800fed572ea32ddfd
Nathaniel Churchill <nchurchill@metrostarsystems.com>
2021-03-03 13:13:03 -0500

    add unit tests

---
+++ CicdApp/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CicdApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestController : ControllerBase
+	{
+
+		[HttpGet]
+		public string GetStuff([FromQuery] string value) 
+		{
+			if (string.IsNullOrEmpty(value))
+			{
+				return "Provide a query param of value";
+
+			}
+			return value.ToUpper();
+		}
+	}
+}
---
+++ CicdAppTests/Controllers/TestControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CicdApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CicdApp.Controllers.Tests
+{
+	[TestClass()]
+	public class TestControllerTests
+	{
+		[TestMethod()]
+		public void GetStuffTest()
+		{
+			TestController controller = new TestController();
+			Assert.AreEqual("TESTME", controller.GetStuff("TestMe"));
+		}
+	}
+}
