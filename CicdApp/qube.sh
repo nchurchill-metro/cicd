c5d55e8c3721f2f093da046c4991d2ca341851e4
Nathaniel Churchill <nchurchill@metrostarsystems.com>
2021-03-09 16:58:27 -0500

    update qube

--- CicdApp/qube.sh
+++ CicdApp/qube.sh
@@ -1,6 +1,6 @@
 
 
 dotnet tool install --global dotnet-sonarscanner
-dotnet sonarscanner begin /k:"project-key"  /d:sonar.login="$AZUREQUBETOKEN"
+dotnet sonarscanner begin /k:"project-key" /d:sonar.host.url="https://sonarqube-pnqi3qjntuz2a.azurewebsites.net": /d:sonar.login="$AZUREQUBETOKEN"
 dotnet build
 dotnet sonarscanner end /d:sonar.login="$AZUREQUBETOKEN"
