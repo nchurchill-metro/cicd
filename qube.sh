0686d7b3e3dc800ff7eb7faf56c7e84bb512e62d
Nathaniel Churchill <nchurchill@metrostarsystems.com>
2021-03-10 16:26:47 -0500

    secret 2

--- qube.sh
+++ qube.sh
@@ -1,6 +1,6 @@
 
 
 dotnet tool install --global dotnet-sonarscanner
-dotnet sonarscanner begin /k:"project-key" /d:sonar.host.url="https://sonarqube-pnqi3qjntuz2a.azurewebsites.net": /d:sonar.login="$TOKEN" /d:sonar.cs.opencover.reportsPaths="/home/vsts/work/1/s/CicdAppTests/TestResults/coverage.opencover.xml" /d:sonar.cs.vstest.reportsPaths="/home/vsts/work/1/s/CicdAppTests/TestResults/results.trx"
+dotnet sonarscanner begin /k:"project-key" /d:sonar.host.url="https://sonarqube-pnqi3qjntuz2a.azurewebsites.net": /d:sonar.login="$(TOKEN)" /d:sonar.cs.opencover.reportsPaths="/home/vsts/work/1/s/CicdAppTests/TestResults/coverage.opencover.xml" /d:sonar.cs.vstest.reportsPaths="/home/vsts/work/1/s/CicdAppTests/TestResults/results.trx"
 dotnet build CicdApp/*.csproj && dotnet test CicdAppTests/*.csproj -l:"trx;LogFileName=results.trx" /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=opencover
-dotnet sonarscanner end /d:sonar.login="$TOKEN"
+dotnet sonarscanner end /d:sonar.login="$(TOKEN)"
