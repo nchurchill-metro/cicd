c8535e152d112a4cc12e9a5816c968be5410b967
Nathaniel Churchill <nchurchill@metrostarsystems.com>
2021-03-10 11:11:51 -0500

    add qude dirs

--- qube.sh
+++ qube.sh
@@ -1,6 +1,6 @@
 
 
 dotnet tool install --global dotnet-sonarscanner
-dotnet sonarscanner begin /k:"project-key" /d:sonar.host.url="https://sonarqube-pnqi3qjntuz2a.azurewebsites.net": /d:sonar.login="$AZUREQUBETOKEN" /d:sonar.cs.opencover.reportsPaths="CicdAppTests/TestResults/coverage.opencover.xml" /d:sonar.cs.vstest.reportsPaths="CicdAppTests/TestResults/results.trx"
+dotnet sonarscanner begin /k:"project-key" /d:sonar.host.url="https://sonarqube-pnqi3qjntuz2a.azurewebsites.net": /d:sonar.login="$AZUREQUBETOKEN" /d:sonar.cs.opencover.reportsPaths="/home/vsts/work/1/s/CicdAppTests/CicdAppTests/TestResults/coverage.opencover.xml" /d:sonar.cs.vstest.reportsPaths="/home/vsts/work/1/s/CicdAppTests/CicdAppTests/TestResults/results.trx"
 dotnet build CicdApp/*.csproj && dotnet test CicdAppTests/*.csproj -l:"trx;LogFileName=results.trx" /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=opencover
 dotnet sonarscanner end /d:sonar.login="$AZUREQUBETOKEN"
